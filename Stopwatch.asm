162978c9da8e87eac1d8ab9b4a97193185542dcb
LabKay <laurabellek@yahoo.com>
2016-02-18 20:17:03 +0000

    Don't merge, just testing stuff

--- Stopwatch.asm
+++ Stopwatch.asm
@@ -14,11 +14,57 @@ ORG   h'0'
     bcf 	STATUS,5  	;select bank 0
     	
 loop
-    movlw	~B'10111111'
+    movlw	B'00001111'
+    movwf	PORTA
+    movlw	~B'10111111'  ;Display all zeros on all displays
     movwf	PORTB
-    BTFSC	PORTA,4
+    BTFSC	PORTA,4  ;When button at port A4 is pushed, go to loop count
     goto	loopcount	
     goto	loop
+    
+seconds     ;Seconds
+    movlw   B'00001000'
+    movwf   PORTA
+    movlw   ~B'01000000'   ;0
+    movwf   PORTB
+	
+    movlw   ~B'10100000'   ;1
+    movwf   PORTB
+;	BTFSC PORTA,4
+;	call loop1
+;	BTFSC PORTB,0
+;	call loopcount
+;	BTFSC PORTA,4
+;	call loop1
+;	BTFSC PORTB,0
+;	call loopcount
+	
+    movlw   ~B'11011010'   ;2
+    movwf   PORTB
+
+    movlw   ~B'11110010'  ;3
+    movwf   PORTB
+
+    movlw   ~B'11100100'   ;4
+    movwf   PORTB
+
+    movlw   ~B'01110110'  ;5
+    movwf   PORTB
+
+    movlw   ~B'01111110'   ;6
+    movwf   PORTB
+
+    movlw   ~B'10100010'  ;7
+    movwf   PORTB
+
+    movlw   ~B'11111110' ;8
+    movwf   PORTB
+
+    movlw   ~B'11100111'  ;9
+    movwf   PORTB
+	
+    goto seconds
+    
 	    
 loop0    
     movlw ~B'00001100'
@@ -100,221 +146,76 @@ loop9
     return
     goto loop9
 
-
-    
-loopcount
-	movlw B'00000001'
-	movwf	PORTA
-	call delayseconds
-	movlw   ~B'01000000'   ;0
-	movwf   PORTB
-        movlw B'00000010'
-        movwf   PORTA
-        call delay01seconds
-        movlw B'01000000'
-        movlw   PORTB
-        movlw B'00000100'
-        movwf   PORTA
-        
-;	BTFSC PORTA,4
-;	call loop0
-;	BTFSC PORTB,0
-;	call loopcount
-    call delay
-;	BTFSC PORTA,4
-;	call loop0
-;	BTFSC PORTB,0
-;	call loopcount
-	
-    call delay
-    movlw   ~B'00001100'   ;1
-    movwf   PORTB
-;	BTFSC PORTA,4
-;	call loop1
-;	BTFSC PORTB,0
-;	call loopcount
-    call delay
-;	BTFSC PORTA,4
-;	call loop1
-;	BTFSC PORTB,0
-;	call loopcount
-	
-    call delay
-    movlw   ~B'11011010'   ;2
-    movwf   PORTB
-;	BTFSC PORTA,4
-;	call loop2
-;	BTFSC PORTB,0
-;	call loopcount
-    call delay
-;	BTFSC PORTA,4
-;	call loop2
-;	BTFSC PORTB,0
-;	call loopcount
-	
-    call delay
-    movlw   ~B'11110010'  ;3
-    movwf   PORTB
-;	BTFSC PORTA,4
-;	call loop3
-;	BTFSC PORTB,0
-;	call loopcount
-    call delay
-;	BTFSC PORTA,4
-;	call loop3
-;	BTFSC PORTB,0
-;	call loopcount
-	
-    call delay
-    movlw   ~B'11100100'   ;4
-    movwf   PORTB
-;	BTFSC PORTA,4
-;	call loop4
-;	BTFSC PORTB,0
-;	call loopcount
-    call delay
-;	BTFSC PORTA,4
-;	call loop4
-;	BTFSC PORTB,0
-;	call loopcount
-	
-    call delay
-    movlw   ~B'01110110'  ;5
-    movwf   PORTB
-;	BTFSC PORTA,4
-;	call loop5
-;	BTFSC PORTB,0
-;	call loopcount
-    call delay
-;	BTFSC PORTA,4
-;	call loop5
-;	BTFSC PORTB,0
-;	call loopcount
-	
-    call delay
-    movlw   ~B'01111110'   ;6
-    movwf   PORTB
-;	BTFSC PORTA,4
-;	call loop6
-;	BTFSC PORTB,0
-;	call loopcount
-    call delay
-;	BTFSC PORTA,4
-;	call loop6
-;	BTFSC PORTB,0
-;	call loopcount
-	
-    call delay
-    movlw   ~B'10100010'  ;7
-    movwf   PORTB
-;	BTFSC PORTA,4
-;	call loop7
-;	BTFSC PORTB,0
-;	call loopcount
-    call delay
-;	BTFSC PORTA,4
-;	call loop7
-;	BTFSC PORTB,0
-;	call loopcount
-	
-    call delay
-    movlw   ~B'11111110' ;8
-    movwf   PORTB
-;	BTFSC PORTA,4
-;	call loop8
-;	BTFSC PORTB,0
-;	call loopcount
-    call delay
-;	BTFSC PORTA,4
-;	call loop8
-;	BTFSC PORTB,0
-;	call loopcount
-	
-    call delay
-    movlw   ~B'11100111'  ;9
-    movwf   PORTB
-;	BTFSC PORTA,4
-;	call loop9
-;	BTFSC PORTB,0
-;	call loopcount
-    call delay
-;	BTFSC PORTA,4
-;	call loop9
-;	BTFSC PORTB,0
-;	call loopcount
-	
-    goto loopcount
-    
     
 ; program delay
 delayseconds
-	movlw       	H'AA'
+    movlw       	H'AA'
   	;initialise delay counters
-	movwf       	DELAY_COUNT1
-	movlw       	H'18'
-	movwf       	DELAY_COUNT2
-	movlw       	H'03'
-	movwf       	DELAY_COUNT3
+    movwf       	DELAY_COUNT1
+    movlw       	H'18'
+    movwf       	DELAY_COUNT2
+    movlw       	H'03'
+    movwf       	DELAY_COUNT3
 delay_loop1
-	decfsz      	DELAY_COUNT1,F  ; innermost loop
-	goto        	delay_loop  	; decrements and loops until delay_count1=0
-	decfsz      	DELAY_COUNT2,F  ; middle loop
-	goto        	delay_loop
-	decfsz      	DELAY_COUNT3,F  ; outer loop
-	goto        	delay_loop
-	return
+    decfsz      	DELAY_COUNT1,F  ; innermost loop
+    goto        	delay_loop  	; decrements and loops until delay_count1=0
+    decfsz      	DELAY_COUNT2,F  ; middle loop
+    goto        	delay_loop
+    decfsz      	DELAY_COUNT3,F  ; outer loop
+    goto        	delay_loop
+    return
 
 delay01seconds
-	movlw       	H'AA'
+    movlw       	H'AA'
   	;initialise delay counters
-	movwf       	DELAY_COUNT1
-	movlw       	H'18'
-	movwf       	DELAY_COUNT2
-	movlw       	H'03'
-	movwf       	DELAY_COUNT3
+    movwf       	DELAY_COUNT1
+    movlw       	H'18'
+    movwf       	DELAY_COUNT2
+    movlw       	H'03'
+    movwf       	DELAY_COUNT3
 delay_loop01
-	decfsz      	DELAY_COUNT1,F  ; innermost loop
-	goto        	delay_loop  	; decrements and loops until delay_count1=0
-	decfsz      	DELAY_COUNT2,F  ; middle loop
-	goto        	delay_loop
-	decfsz      	DELAY_COUNT3,F  ; outer loop
-	goto        	delay_loop
-	return
+    decfsz      	DELAY_COUNT1,F  ; innermost loop
+    goto        	delay_loop  	; decrements and loops until delay_count1=0
+    decfsz      	DELAY_COUNT2,F  ; middle loop
+    goto        	delay_loop
+    decfsz      	DELAY_COUNT3,F  ; outer loop
+    goto        	delay_loop
+    return
 
 delay001seconds
-	movlw       	H'AA'
+    movlw       	H'AA'
   	;initialise delay counters
-	movwf       	DELAY_COUNT1
-	movlw       	H'18'
-	movwf       	DELAY_COUNT2
-	movlw       	H'03'
-	movwf       	DELAY_COUNT3
+    movwf       	DELAY_COUNT1
+    movlw       	H'18'
+    movwf       	DELAY_COUNT2
+    movlw       	H'03'
+    movwf       	DELAY_COUNT3
 delay_loop001
-	decfsz      	DELAY_COUNT1,F  ; innermost loop
-	goto        	delay_loop  	; decrements and loops until delay_count1=0
-	decfsz      	DELAY_COUNT2,F  ; middle loop
-	goto        	delay_loop
-	decfsz      	DELAY_COUNT3,F  ; outer loop
-	goto        	delay_loop
-	return
+    decfsz      	DELAY_COUNT1,F  ; innermost loop
+    goto        	delay_loop  	; decrements and loops until delay_count1=0
+    decfsz      	DELAY_COUNT2,F  ; middle loop
+    goto        	delay_loop
+    decfsz      	DELAY_COUNT3,F  ; outer loop
+    goto        	delay_loop
+    return
 
 
 delay0001seconds
-	movlw       	H'AA'
+    movlw       	H'AA'
   	;initialise delay counters
-	movwf       	DELAY_COUNT1
-	movlw       	H'18'
-	movwf       	DELAY_COUNT2
-	movlw       	H'03'
-	movwf       	DELAY_COUNT3
+    movwf       	DELAY_COUNT1
+    movlw       	H'18'
+    movwf       	DELAY_COUNT2
+    movlw       	H'03'
+    movwf       	DELAY_COUNT3
 delay_loop0001
-	decfsz      	DELAY_COUNT1,F  ; innermost loop
-	goto        	delay_loop  	; decrements and loops until delay_count1=0
-	decfsz      	DELAY_COUNT2,F  ; middle loop
-	goto        	delay_loop
-	decfsz      	DELAY_COUNT3,F  ; outer loop
-	goto        	delay_loop
-	return
+    decfsz      	DELAY_COUNT1,F  ; innermost loop
+    goto        	delay_loop  	; decrements and loops until delay_count1=0
+    decfsz      	DELAY_COUNT2,F  ; middle loop
+    goto        	delay_loop
+    decfsz      	DELAY_COUNT3,F  ; outer loop
+    goto        	delay_loop
+    return
 
 delay2
     movlw       	H'08'
